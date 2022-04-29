b62cbf4c5e509a75941e73291f8ef8c55e071b1e
Baruch Odem <baruchiro@gmail.com>
2022-04-29 14:48:43 +0000

    fix

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = ({ search, setSearch }) => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" className="mw-100" />
         <h2>
-          A place to get
+          A place to <span onClick={() => setVisible((i) => !i)}>get</span>
+          &nbsp;
           <input
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             id="search-box"
             placeholder="What is it that you truly desire?"
-            style={{ fontSize: "small" }}
+            style={{ fontSize: "small", display: visible ? undefined : "none" }}
           />
           the cool stuff.
         </h2>
